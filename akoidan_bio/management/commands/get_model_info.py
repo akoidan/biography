7f95ece90c23abf9806ab0b2eff9563e3468a1b8
Andrey Koidan <ext_pnn_andrey.koidan@life.com.ua>
2015-03-04 17:20:46 +0200

    added a command that prints every single model and its fields count

---
+++ akoidan_bio/management/commands/get_model_info.py
@@ -0,0 +1,14 @@
+__author__ = 'andrew'
+from django.core.management import BaseCommand
+from django.db import models
+
+
+class Command(BaseCommand):
+    help = 'Prints all project models and counts objects in the each one'
+
+    can_import_settings = True
+
+    def handle(self, *args, **options):
+        all_models = models.get_models(include_auto_created=True)
+        for member in all_models:
+            self.stdout.write('%s has %d fields' % (member.__name__, len(member._meta.fields)))
--- akoidan_bio/templatetags/links.py
+++ akoidan_bio/templatetags/links.py
@@ -8,5 +8,6 @@ register = template.Library()
 
 @register.filter(name="render_admin_link")
 def get_object_link(object):
+    # TODO get ip and port from request
     return 'http://127.0.0.1:8000/admin/akoidan_bio/%s/%d' % (object.__class__.__name__.lower(), object.pk)
 
