1d8c9245dc3bbf5b76cecd7fdd204da0678cf90a
Andrey Koidan <ext_pnn_andrey.koidan@life.com.ua>
2015-03-04 17:48:26 +0200

    Create django management command that prints all project models and the count of objects in every model. Also: ● Duplicate output to STDERR with prefix "error: " ● Write a bash script which executes your command and saves output of stderr into file. File name should be “<current date>.log”

--- akoidan_bio/management/commands/get_model_info.py
+++ akoidan_bio/management/commands/get_model_info.py
@@ -1,3 +1,5 @@
+import sys
+
 __author__ = 'andrew'
 from django.core.management import BaseCommand
 from django.db import models
@@ -11,4 +13,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         all_models = models.get_models(include_auto_created=True)
         for member in all_models:
-            self.stdout.write('%s has %d fields' % (member.__name__, len(member._meta.fields)))
+            output = '%s has %d fields' % (member.__name__, len(member._meta.fields))
+            self.stdout.write(output)
+            self.stderr.write('error: %s' % output)
---
+++ get_models.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 manage.py get_model_info 2> `date +"%k-%M-%S"`.log
