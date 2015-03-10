c9d37b0ee210f397ea887eb07863287bea4c7d52
deathangel908 <nightmare.quake@mail.com>
2015-03-11 00:22:32 +0200

    added require_http_method annotation

--- akoidan_bio/views.py
+++ akoidan_bio/views.py
@@ -1,14 +1,14 @@
 __author__ = 'andrew'
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.core.context_processors import csrf
-from django.core.exceptions import PermissionDenied, ObjectDoesNotExist, ValidationError
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from akoidan_bio.forms import UserProfileForm
 from akoidan_bio.models import UserProfile, Request
-from django.http import Http404
 from akoidan_bio.reg_auth_utils import validate_user, validate_password
 from akoidan_bio.settings import REQUESTS_COUNT, DEFAULT_PROFILE_ID
 from django.contrib.auth import get_user_model
@@ -49,42 +49,38 @@ def requests(request):
 
 
 @login_required
+@require_http_methods("POST")
 def change_form(request):
     """
     Accepts a UserProfileForm and saves it if it's validate
     """
-    if request.method == 'POST':
-        user_profile = UserProfile.objects.get(pk=request.user.id)
-        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/')
-        else:
-            return render_to_response("akoidan_bio/response.html",
-                                      {'message': form.errors},
-                                      context_instance=RequestContext(request))
+    user_profile = UserProfile.objects.get(pk=request.user.id)
+    form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/')
     else:
-        raise Http404
+        return render_to_response("akoidan_bio/response.html",
+                                  {'message': form.errors},
+                                  context_instance=RequestContext(request))
 
 
+@require_http_methods("POST")
 def auth(request):
     """
     Logs in into system.
     """
-    if request.method == 'POST':
-        username = request.POST['login']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect("/")
-        else:
-            message = "Login or password is wrong"
-        return render_to_response("akoidan_bio/response.html",
-                                  {'message': message},
-                                  context_instance=RequestContext(request))
+    username = request.POST['login']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect("/")
     else:
-        raise Http404
+        message = "Login or password is wrong"
+    return render_to_response("akoidan_bio/response.html",
+                              {'message': message},
+                              context_instance=RequestContext(request))
 
 
 @login_required
@@ -93,25 +89,23 @@ def log_out(request):
     return HttpResponseRedirect('/')
 
 
+@require_http_methods("POST")
 def register(request):
     """
     Accepts a Registration form and registers a new user if form is valid.
     """
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        try:
-            validate_user(username)
-            validate_password(password)
-            user = get_user_model().objects.create_user(username=username, password=password)
-            user.save()
-            authed_user = authenticate(username=username, password=password)
-            login(request, authed_user)
-            message = 'you successfully registered'
-        except ValidationError as e:
-            message = e.message
-        return render_to_response("akoidan_bio/response.html",
-                                  {'message': message},
-                                  context_instance=RequestContext(request))
-    else:
-        raise Http404
+    username = request.POST['username']
+    password = request.POST['password']
+    try:
+        validate_user(username)
+        validate_password(password)
+        user = get_user_model().objects.create_user(username=username, password=password)
+        user.save()
+        authed_user = authenticate(username=username, password=password)
+        login(request, authed_user)
+        message = 'you successfully registered'
+    except ValidationError as e:
+        message = e.message
+    return render_to_response("akoidan_bio/response.html",
+                              {'message': message},
+                              context_instance=RequestContext(request))
