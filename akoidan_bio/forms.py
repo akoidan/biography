d9adc0face7f6d76dab3c50a3a31add8c9d65dee
Andrey Koidan <ext_pnn_andrey.koidan@life.com.ua>
2015-03-10 10:34:41 +0200

    Added simple custom widget

--- akoidan_bio/forms.py
+++ akoidan_bio/forms.py
@@ -1,24 +1,40 @@
 __author__ = 'andrew'
+from django.utils.safestring import mark_safe
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-from django.forms import ImageField, DateField
+from django.forms import ImageField, CharField
 from django import forms
 from akoidan_bio.models import UserProfile
+from string import Template
+
+
+class ContactsWidget(forms.widgets.Textarea):
+    """
+    The simple widget that renders the same html as the default one
+    """
+    def render(self, name, value, attrs=None):
+        html = Template("""
+        <input class="textinput textInput" id="id_contacts" maxlength="100" name="contacts" type="text" value="$value">
+        """)
+        return mark_safe(html.substitute(value=value))
 
 
 class UserProfileForm(forms.ModelForm):
     """
     A form that provides a way to edit UserProfile
     """
-    birth_date = DateField()
     # the widget gets rid of <a href=
     photo = ImageField(widget=forms.FileInput)
+    contacts = CharField(widget=ContactsWidget)
 
     class Meta:  # pylint: disable=C1001
         model = UserProfile
         fields = ('name', 'surname', 'birth_date', 'contacts', 'bio', 'photo')
 
     def __init__(self, *args, **kwargs):
+        """
+        Creates the entire form for changing UserProfile.
+        """
         self.helper = FormHelper()
         self.helper.form_action = '/form'
 
